4932d2d194ac7f1a796db1ff31f6a7c6378c4b7e
Missing_Love <0xwymx0@gmail.com>
2025-05-12 05:05:41 +0800

    修复

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
+// @ts-ignore
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
